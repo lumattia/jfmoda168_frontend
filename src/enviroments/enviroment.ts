a48a54804e97fd6a1f1768ac759a70ef893b8784
lumattia <lumattia88@gmail.com>
2025-09-10 19:45:51 +0200

    enviroment

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/v1/api',
-  fileUrl: 'http://localhost:8080'
+  apiUrl: 'http://192.168.1.8:8080/v1/api',
+  fileUrl: 'http://192.168.1.8:8080'
 };
